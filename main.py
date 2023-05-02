b027cd09a8ce8c7fe9b139be093e4b8f5698bbda
TazwellJ <72585986+TazwellJ@users.noreply.github.com>
2023-05-02 13:20:18 -0400

    Create main.py
    Initial file creation

---
+++ main.py
@@ -0,0 +1,9 @@
+from machine import Pin,I2C
+import chirp # library for the moisture sensors
+import utime
+import _thread # used to run the reservoir routine on one CPU and moisture sensors/pumps on the other CPU
+import secrets # will contain your wifi and ThingSpeak credentials
+import wifi
+
+This is in progress.  Will upload actual code this evening.
+
