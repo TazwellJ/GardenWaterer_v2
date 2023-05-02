1bcce094fa77eeceb2e3d56c023d230f88546ee7
TazwellJ <72585986+TazwellJ@users.noreply.github.com>
2023-05-02 17:26:56 -0400

    Update secrets.py

--- secrets.py
+++ secrets.py
@@ -1,3 +1,6 @@
-wifi_ssid = ""
-wifi_pass = ""
+wifi_ssid = '<your_wifi_ssid>'
+wifi_pass = '<your_wifi_pass>'
+
+HTTP_HEADERS = {'Content-Type': 'application/json'}
+THINGSPEAK_WRITE_API_KEY = '<your_ThingSpeak_API_key'
 
