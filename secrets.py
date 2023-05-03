5a0a823df74752cc1f1241d4198caa9b562d9953
TazwellJ <72585986+TazwellJ@users.noreply.github.com>
2023-05-02 20:32:35 -0400

    Update secrets.py

--- secrets.py
+++ secrets.py
@@ -1,5 +1,5 @@
 secrets = {
     "wifi_ssid" : "Your wifi SSID",
     "wifi_pass": "Your wifi password",
-    "thingspeak_api_key": "Your API key from ThingSpeak.com"
+    "ts_api_key": "Your API key from ThingSpeak.com"
 }
