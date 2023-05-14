9a68ea985e923a8671065ddb4269d119d47f0216
TazwellJ <72585986+TazwellJ@users.noreply.github.com>
2023-05-14 11:45:41 -0400

    Add files via upload
    Modularized the wifi connection so streamline main code

--- lib/wifi.py
+++ lib/wifi.py
@@ -1,16 +1,42 @@
 import network
 
-def connect_wifi(ssid, password):
-    #Connect to your network
-    station = network.WLAN(network.STA_IF)
-    station.active(True)
-    station.connect(ssid, password)
-    while station.isconnected() == False:
-        pass
-    print('Connection successful')
-    print(station.ifconfig())
+class WiFi:
+
+    def __init__(self,ssid,password):
+        self.ssid = ssid
+        self.password = password      
+        self.station = network.WLAN(network.STA_IF)
+        
+    def connect_wifi(self):
+        #Connect to your network
+        self.station.active(True)
+        self.station.connect(self.ssid, self.password)
+        while self.station.isconnected() == False:
+            pass
+        print('Connection successful')
+        print(self.station.ifconfig())
   
-def wifi_status():
-    station = network.WLAN(network.STA_IF)
-    return station.isconnected()
-   
+    def wifi_status(self):
+        # Make sense of the wlan.status() numbers that are returned
+        status = self.station.status()
+        if status == network.STAT_IDLE:
+            return 'STAT_IDLE'
+        elif status == network.STAT_CONNECTING:
+            return 'STAT_CONNECTING'
+        elif status == network.STAT_WRONG_PASSWORD:
+            return 'STAT_WRONG_PASSWORD'
+        elif status == network.STAT_NO_AP_FOUND:
+            return 'STAT_NO_AP_FOUND'
+        elif status == network.STAT_CONNECT_FAIL:
+            return 'STAT_CONNECT_FAIL'
+        elif status == network.STAT_GOT_IP:
+            return 'STAT_GOT_IP'
+        else:
+            return "Unknown wlan status: {}".format(status)
+    
+    def connected(self):
+        if self.station.isconnected() == True:
+            return True
+        else:
+            return False
+        
